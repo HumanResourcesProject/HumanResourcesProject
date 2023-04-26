408d7785510df31cf83c728903ab487ca8330b0b
busecnky <cankayabuse94@gmail.com>
2023-04-26 15:44:07 +0300

    second commit

---
+++ AdminMicroService/src/main/java/com/hrp/AdminMicroServiceApplication.java
@@ -0,0 +1,11 @@
+package com.hrp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminMicroServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AdminMicroServiceApplication.class);
+    }
+}
