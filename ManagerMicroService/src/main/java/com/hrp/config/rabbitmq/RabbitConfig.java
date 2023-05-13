a3ccaef1284595afbb18b80e4925eaa22f737d6d
mertnamsal <namsalmert@gmail.com>
2023-05-14 01:54:47 +0300

    düzeltme

--- ManagerMicroService/src/main/java/com/hrp/config/rabbitmq/RabbitConfig.java
+++ ManagerMicroService/src/main/java/com/hrp/config/rabbitmq/RabbitConfig.java
@@ -14,12 +14,19 @@ public class RabbitConfig {
 
     // Key
     private String adminEmailKey = "admin-email-key";
+    private String bindingKeyRegisterAdmin= "binding-key-register-admin" ;
+    private String bindingKeyRegisterManager= "binding-key-register-manager" ;
+    private String bindingKeyRegisterEmployee= "binding-key-register-employee" ;
 
 
 
 
     // Queue
     private  String adminEmailQueue = "admin-email-queue";
+    private  String queueRegisterAdmin = "queue-register-admin";
+    private  String queueRegisterManager = "queue-register-manager";
+    private  String queueRegisterEmployee= "queue-register-employee";
+
 
 
     /**
@@ -51,6 +58,20 @@ public class RabbitConfig {
     Queue adminEmailQueue(){
         return new Queue(adminEmailQueue);
     }
+    @Bean
+    Queue queueRegisterAdmin(){
+        return new Queue(queueRegisterAdmin);
+    }
+
+    @Bean
+    Queue queueRegisterManager(){
+        return new Queue(queueRegisterManager);
+    }
+
+    @Bean
+    Queue queueRegisterEmployee(){
+        return new Queue(queueRegisterEmployee);
+    }
 
 
     /**
@@ -60,6 +81,20 @@ public class RabbitConfig {
     public Binding bindingAdminMail(final Queue adminEmailQueue ,final DirectExchange directExchange ){
         return BindingBuilder.bind(adminEmailQueue).to(directExchange).with(adminEmailKey);
     }
+    @Bean
+    public Binding bindingRegisterAdmin(final Queue queueRegisterAdmin,final DirectExchange directExchange){
+        return BindingBuilder.bind(queueRegisterAdmin).to(directExchange).with(bindingKeyRegisterAdmin);
+    }
+    @Bean
+    public Binding bindingRegisterManager(final Queue queueRegisterManager,final DirectExchange directExchange){
+        return BindingBuilder.bind(queueRegisterManager).to(directExchange).with(bindingKeyRegisterManager);
+    }
+    @Bean
+    public Binding bindingRegisterEmployee(final Queue queueRegisterEmployee,final DirectExchange directExchange){
+        return BindingBuilder.bind(queueRegisterEmployee).to(directExchange).with(bindingKeyRegisterEmployee);
+    }
+
+
 
 
 }
